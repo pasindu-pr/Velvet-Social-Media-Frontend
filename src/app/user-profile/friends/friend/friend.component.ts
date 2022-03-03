2e4e0fe7ad3df16e8912cfcb32848b6eb21dd7f9
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2022-03-03 12:33:24 +0530

    Implement user unfriend in client side

--- src/app/user-profile/friends/friend/friend.component.ts
+++ src/app/user-profile/friends/friend/friend.component.ts
@@ -1,6 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import * as dayjs from 'dayjs';
 import * as relativeTime from 'dayjs/plugin/relativeTime';
+import { ToastrService } from 'ngx-toastr';
+import { ApplicationState } from 'src/app/redux/reducers';
+import { environment } from 'src/environments/environment';
+import * as FriendsActions from '../../../redux/actions/friends.action';
 
 @Component({
   selector: 'app-friend',
@@ -8,7 +14,11 @@ import * as relativeTime from 'dayjs/plugin/relativeTime';
   styleUrls: ['./friend.component.scss'],
 })
 export class FriendComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+    private store: Store<ApplicationState>,
+    private toastService: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -16,9 +26,32 @@ export class FriendComponent implements OnInit {
   @Input() name: string;
   @Input() location: string;
   @Input() createdAt: string;
+  @Input() id: number;
 
   getFriendFromDate() {
     dayjs.extend(relativeTime);
     return dayjs(this.createdAt).fromNow();
+  }
+
+  onUnfriendClick() {
+    this.http
+      .delete(`${environment.backendUrl}/socialmedia/friends/${this.id}/`, {
+        observe: 'response',
+      })
+      .subscribe((response) => {
+        if (response.status == 204) {
+          this.toastService.info(
+            'Friend unfriend',
+            `You unfriended ${this.name} successfully!`,
+            {
+              progressBar: true,
+              positionClass: 'toast-bottom-right',
+              closeButton: true,
+              timeOut: 5000,
+            }
+          );
+          this.store.dispatch(FriendsActions.FETCH_USERS_FRIENDS_REQUEST());
+        }
+      });
   }
 }
